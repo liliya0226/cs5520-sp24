dc28bbe653126cf8988a91469b7b9973a2dd9f46
李明曦 <limingxi@limingxis-MacBook-Air.local>
2024-01-09 14:59:45 -0800

    initial version

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HELLO YUEGUI!🐶</Text>
+   <StatusBar style="auto" />   
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
